c0b7681d992eef62b61c763abaebb518e3a2bbbd
Hernán Darío Metaute Sarmiento <hernan.metaute@gmail.com>
2013-03-06 20:45:59 -0500

    borrado

---
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -0,0 +1,28 @@
+package com.develogical;
+
+public class QueryProcessor {
+
+    public String process(String query) {
+        if (query.contains("which of the following numbers is the largest:")) {
+        	String partido[] = query.split(": ");
+        	String numeros[] = partido[1].split(" ");
+        	Integer primerNumero = Integer.parseInt(numeros[0]);
+        	Integer segundoNumero = Integer.parseInt(numeros[1]);
+        	
+        	return Math.max(primerNumero, segundoNumero) + "";
+            
+        }else if(query.contains("plus")){
+        	String partido[] = query.split("what is ");
+        	String numeros[] = partido[1].split(" plus ");
+        	Integer primerNumero = Integer.parseInt(numeros[0]);
+        	Integer segundoNumero = Integer.parseInt(numeros[1]);
+        	return (primerNumero + segundoNumero) + "";
+        }else if(query.contains("what is your name")){
+        	return "mingas";
+        }
+        
+        return "";
+        
+    }
+
+}
---
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -0,0 +1,42 @@
+package com.develogical;
+
+import org.junit.Test;
+
+import com.develogical.QueryProcessor;
+
+import java.util.Map;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+public class QueryProcessorTest {
+
+    @Test
+    public void largestNumber() {
+        String result = new QueryProcessor().process("which of the following numbers is the largest: 46 31");
+        assertNotNull(result);
+        assertThat(result, is("46"));
+    }
+    
+    @Test
+    public void canSumTwoNumbers() {
+        String result = new QueryProcessor().process("what is 20 plus 15");
+        assertNotNull(result);
+        assertThat(result, is("35"));
+    }
+    
+    @Test
+    public void canGreetYou(){
+    	String result = new QueryProcessor().process("what is your name");
+        assertNotNull(result);
+        assertThat(result, is("mingas"));
+    }
+
+
+
+}
+
+
+
