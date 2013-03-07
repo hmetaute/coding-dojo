e3fe8f9c1585d8f7daf89283b2c0d2bd010b22ad
Hernán Darío Metaute Sarmiento <hernan.metaute@gmail.com>
2013-03-06 20:09:07 -0500

    my name is

---
+++ nuevo/com/develogical/QueryProcessor.java
@@ -0,0 +1,15 @@
+package com.develogical;
+
+public class QueryProcessor {
+
+    public String process(String query) {
+        if (query.contains("plus")) {
+        	String sumando[] = query.split(" plus ");
+        	
+        	
+            return "mingas";
+        }
+        return "mingas";
+    }
+
+}
