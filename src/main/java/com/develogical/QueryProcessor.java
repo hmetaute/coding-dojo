e1a7202126d5b4861c0347db16c35109114be104
Hernán Darío Metaute Sarmiento <hernan.metaute@gmail.com>
2013-03-06 19:57:04 -0500

    mingas

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -4,9 +4,9 @@ public class QueryProcessor {
 
     public String process(String query) {
         if (query.contains("what is your name")) {
-            return "Los Mingas 2";
+            return "mingas";
         }
-        return "Los Mingas 2";
+        return "mingas";
     }
 
 }
