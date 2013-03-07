ff1b4a60304b516e6951544fbd28250aa6336208
Hernán Darío Metaute Sarmiento <hernan.metaute@gmail.com>
2013-03-06 20:55:33 -0500

    hernan no hace commit

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -5,7 +5,7 @@ public class QueryProcessor {
     public String process(String query) {
         if (query.contains("which of the following numbers is the largest:")) {
         	String partido[] = query.split(": ");
-        	String numeros[] = partido[1].split(" ");
+        	String numeros[] = partido[2].split(" ");
         	Integer primerNumero = Integer.parseInt(numeros[0]);
         	Integer segundoNumero = Integer.parseInt(numeros[1]);
         	
